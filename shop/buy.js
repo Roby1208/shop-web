4848ed91d2e69ffaae04d306c838db645695a3cd
Roby1208 <triro299@energie-pass.lu>
2019-06-18 10:53:29 +0200

    basket

--- shop/buy.js
+++ shop/buy.js
@@ -1,3 +1,9 @@
 function buy{
-    alert("Hell0 " + Firstname + Lastname + ", " + "<br>" + Basket + "is beeing send to " + Address + country + "<br>" + "with" + Extra)
+    var firstname = document.getElementById(Firstname).value;
+    var lastname = document.getElementById(Lastname).value;
+    var basket = document.getElementById(Basket).value;
+    var address = document.getElementById(Address).value;
+    var country = document.getElementById(Country).value;
+    var extra = document.getElementById(Extra).value;
+    alert("Hell0 " + firstname + lastname + ", " + "<br>" + basket + "is beeing send to " + address + country + "<br>" + "with" + extra)
 }
--- shop/shop.js
+++ shop/shop.js
@@ -1,7 +1,23 @@
+var shoe = "gggg";
+
+
+
+
+
 function openNav() {
     document.getElementById("myNav").style.display = "block";
   }
   
   function closeNav() {
     document.getElementById("myNav").style.display = "none";
+  }
+
+  function buy(url,name,shoeName){
+    shoe = shoeName;
+    var newWindow = window.open(url,name);
+
+
+  }
+  function display(){
+    document.getElementById("Basket").value = shoe;
   }
