a57ef27f788d8db1ed7f45fcbd0d56b67a7d6705
Roby1208 <triro299@energie-pass.lu>
2019-06-20 09:50:18 +0200

    nerly tghere

--- shop/shop.js
+++ shop/shop.js
@@ -1,4 +1,4 @@
-var shoe = "";
+
 
 
 
@@ -12,27 +12,53 @@ function openNav() {
     document.getElementById("myNav").style.display = "none";
   }
 
-  function buy(url,name,shoeName){
-    shoe = shoeName;
-    var newWindow = window.open(url,name);
+  var myArray = [];
+
+        function add(shoeName) {
+            if (shoeName === "") {
+                alert("Please enter a value");
+            } else {
+                myArray.push(shoeName);
+            }
+
+            document.getElementById("Basket").innerHTML = myArray;
+        }
+
+        function buy(){
+            var Firstname = document.getElementById('Firstname').value;
+            var Lastname = document.getElementById('Lastname').value;
+            var Basket = document.getElementById('Basket').value;
+            var Address = document.getElementById('Address').value;
+            var Country = document.getElementById('Country').value;
+
+            alert(Firstname + " " + Lastname + Basket + " will be delivered to " + Address + Country);
+        }
+
+        var myIndex = 0;
+        carousel();
+        
+        function carousel() {
+          var i;
+          var x = document.getElementsByClassName("slideShow");
+          for (i = 0; i < x.length; i++) {
+             x[i].style.display = "none";
+          }
+          myIndex++;
+          if (myIndex > x.length) {myIndex = 1}
+          x[myIndex-1].style.display = "block";
+          setTimeout(carousel, 3000);
+        }
 
 
-  }
-  function display(){
-    document.getElementById("Basket").value = shoe;
-  }
 
-  var myIndex = 0;
-  
-  
-  function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    }
-    myIndex++;
-    if (myIndex > x.length) {myIndex = 1}
-    x[myIndex-1].style.display = "block";
-    setTimeout(carousel, 3000);
-  }
+
+
+
+
+ // function buy(url,name,shoeName){
+ //   shoe = shoeName;
+  //  var newWindow = window.open(url,name);
+//  }
+ // function display(){
+  //  document.getElementById("Basket").value = shoe;
+ // }
